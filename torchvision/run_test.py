9f692ef414a896477732c2aca198a452e166b39b
Samantha Staheli <64183594+samanthastaheli@users.noreply.github.com>
2024-01-31 13:09:31 -0700

    Create run_test.py

---
+++ torchvision/run_test.py
@@ -0,0 +1,15 @@
+"""
+runs the resnet model python file to log what is called
+- finctions
+- classes
+- imported python files
+- python packages
+"""
+from models.resnet import ResNet
+from logger import Log
+
+def main():
+  pass
+
+if __name__ == main()
+  main()
