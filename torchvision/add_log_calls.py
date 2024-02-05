df9f27984352ca62098640601dec8ea72b6006a1
Samantha Staheli <64183594+samanthastaheli@users.noreply.github.com>
2024-02-05 11:09:41 -0700

    Update add_log_calls.py

--- torchvision/add_log_calls.py
+++ torchvision/add_log_calls.py
@@ -3,8 +3,15 @@
 # test calls to look for
 
 # Log.write_call(currentframe(), "") 
-calls = ["
+calls = ["ImageClassification", "_log_api_usage_once", 
+         "register_model", "Weights", "WeightsEnum", 
+         "_IMAGENET_CATEGORIES", "_ovewrite_named_param", 
+         "handle_legacy_interface"]
+
 py_file = os.open("models/resnet.py", "a")
 file_lines = os.readlines(py_file)
 
-for call in 
+for call in calls:
+  for line in file_lines:
+    if call in line:
+      
